8c8d0a38306267bdca0c3dc7363ebfe36f123170
salmanferoz <salmanferoz@folio3.com>
2025-09-21 11:36:18 +0500

    Initial commit: Add Python agent project with Gemini API integration

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from hello-agent!")
+from os import name
+from agents import Agent,Runner,OpenAIChatCompletionsModel, AsyncOpenAI
+import os
+from dotenv import load_dotenv,find_dotenv
+
+load_dotenv(find_dotenv());
 
 
-if __name__ == "__main__":
-    main()
+
+# 1. Which LLM Service?
+external_client: AsyncOpenAI = AsyncOpenAI(
+    api_key=os.getenv("GEMINI_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+# 2. Which LLM Model?
+llm_model: OpenAIChatCompletionsModel = OpenAIChatCompletionsModel(
+    model="gemini-2.5-flash",
+    openai_client=external_client
+)
+
+agent:Agent = Agent(
+    name = "Assistant",
+    #instructions = "You are a assistant agent",
+    model=llm_model
+)
+
+result = Runner.run_sync(agent,"Hello")
+print(result.final_output)
